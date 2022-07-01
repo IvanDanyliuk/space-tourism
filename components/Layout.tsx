d7ab7cc7da17aceb72f27def2065a838e69ac4b2
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-07-01 21:53:21 +0300

    Layout and Navbar components have been implemented

---
+++ components/Layout.tsx
@@ -0,0 +1,30 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from './Navbar';
+
+
+interface ILayout {
+  children: React.ReactNode;
+};
+
+
+const Main = styled.main`
+  position: relative;
+  height: 100vh;
+  font-family: 'Bellefair', serif;
+`;
+
+
+const Layout: React.FC<ILayout> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <Main>
+        { children }
+      </Main>
+    </>
+  );
+};
+
+export default Layout;
---
+++ components/Navbar.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import styled from 'styled-components';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../public/assets/img/desktop/logo.png'
+
+
+const Header = styled.header`
+  padding-left: 60px;
+  position: absolute;
+  top: 20px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-family: 'Barlow Condensed', sans-serif;
+  box-sizing: border-box;
+  z-index: 100;
+`;
+
+const Logo = styled(Box)``;
+
+const NavMenu = styled.nav`
+  padding: 30px 110px 30px 120px;
+  background: rgba(11, 13, 23, 0.1);
+  backdrop-filter: blur(10px);
+`;
+
+const NavList = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  
+`;
+
+const NavItem = styled.li`
+  margin-right: 50px;
+  cursor: pointer;
+  list-style: none;
+  text-transform: uppercase;
+  color: rgb(255, 255, 255);
+`;
+
+const LinkNum = styled.span`
+  margin-right: 10px;
+  font-weight: 700;
+`;
+
+const LinkName = styled.span``;
+
+
+const Navbar = () => {
+  return (
+    <Header>
+      <Logo>
+        <Image src={logo} alt='logo' />
+      </Logo>
+      <NavMenu>
+        <NavList>
+          <NavItem>
+            <Link href='/'>
+              <Typography variant='inherit'>
+                <LinkNum>00</LinkNum>
+                <LinkName>Home</LinkName>
+              </Typography>
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link href='/destination'>
+              <Typography variant='inherit'>
+                <LinkNum>01</LinkNum>
+                <LinkName>Destination</LinkName>
+              </Typography>
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link href='/crew'>
+              <Typography variant='inherit'>
+                <LinkNum>02</LinkNum>
+                <LinkName>Crew</LinkName>
+              </Typography>
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link href='/technology'>
+              <Typography variant='inherit'>
+                <LinkNum>03</LinkNum>
+                <LinkName>Technology</LinkName>
+              </Typography>
+            </Link>
+          </NavItem>
+        </NavList>
+      </NavMenu>
+    </Header>
+  )
+};
+
+export default Navbar;
